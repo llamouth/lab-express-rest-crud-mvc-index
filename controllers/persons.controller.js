ccea778ec2dee4d4e53fb03c6ab592f268947e77
llamouth <150949923+llamouth@users.noreply.github.com>
2024-06-24 15:23:05 -0400

    feat: Complete lab

--- controllers/persons.controller.js
+++ controllers/persons.controller.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const persons = express.Router()
-const personsArray = require("../models/person.model")
 const {organizePeopleWithLocations} = require("../utils/helpers.js")
 
 persons.get("/", (req, res) => {
